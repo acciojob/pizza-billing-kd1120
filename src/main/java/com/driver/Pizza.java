e0143ed66c94bac5ed1ffe03361ed2ea1831a19b
khushboo <khushidudeja45@gmail.com>
2023-06-15 20:25:02 +0530

    Assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,63 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
-    private Boolean isVeg;
+    private boolean isVeg;
+
+    private int totalPrice;
+
+    private int extraCheesePrice;
+
+    private int paperBagPrice;
+
+    private int toppingsPrice;
+
     private String bill;
 
-    public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
-    }
+    Pizza(boolean isVeg){
 
-    public int getPrice(){
-        return this.price;
+        if(isVeg){
+            totalPrice = 300;
+            this.bill = "Base Price of Veg Pizza :"+300 +"\n";
+        }else{
+            totalPrice = 400;
+            this.bill = "Base Price Of Non Veg Pizza : "+400 +"\n";
+
+        }
+        this.extraCheesePrice = 80;
+        this.paperBagPrice = 20;
+
+        if(isVeg){
+            this.toppingsPrice = 70;
+        }else{
+            this.toppingsPrice = 120;
+        }
+
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        totalPrice = totalPrice + extraCheesePrice;
+
+        this.bill = this.bill + "Extra cheese added :"+extraCheesePrice+"\n";
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        totalPrice = totalPrice + toppingsPrice;
+        this.bill = this.bill + "Extra Toppings added :"+toppingsPrice+"\n";
     }
 
-    public void addTakeaway(){
-        // your code goes here
+    public void paperBagAdded(){
+        totalPrice = totalPrice + paperBagPrice;
+        this.bill = this.bill + "Paper Bag added :"+paperBagPrice+"\n";
+    }
+
+    public int getTotalPrice(){
+        return totalPrice;
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        this.bill = this.bill + "Total Bill Price is :"+totalPrice+"\n";
+        return bill;
     }
+
+
 }
